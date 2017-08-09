0585cb1edf5e59d1e59c7a05ab390432e06764fb
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-09 16:16:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var articleOne= {
                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
                    </p> `
 };
-function createTemplatee (data) {
+function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
