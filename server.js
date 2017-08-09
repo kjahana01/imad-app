c1d6adc333b349eccfecea6cae3f05132005898a
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-09 18:27:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,88 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-   'article-one': {
-    title: 'Article one i Kushal',
-    heading: 'Article one',
-    date: '4th augest 2017',
-    content: `
-                  <p>
-                   This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                   </p>
-                   
-                   <p>
+var artickleOne = {
+  title : 'Article one  I m kushal',
+ heading: 'Artcle one',
+ date: 'Aug 4th 2017',
+ content: `
+         <p>
                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
                    </p>
                    <p>
                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                   </p> 
-};
-  
-
+                   </p>
+                   <p>
 function createTemplate (data) {
     var title = data.title;
-    var date = data.date;
+    var date = date.date;
     var heading = data.heading;
     var content = data.content;
-    
-   var htmlTemplate = '
- <html>
-      <head>
-          <Title>
-              ${title}
-          </Title>
-          <meta name="vewport" content="width=device-width,initial-scale=1"/>
-          <link href="/ui/style.css" rel="stylesheet" />
-          
-    </head>
-    <body>
-        <div class="container">
-        <div> 
-           <a href="/">Home</a>
-         </div>
-    <br/>
-       <h3>
-        ${heading}
-       </h3>
-        <div>
-          ${date}
-         </div>
+
+    var htmlTemplate = `
+    <html>
+          <head>
+              <Title>
+                 ${title}
+              </title>
+              <meta name="vewport" content="width=device-width,initial-scale=1"/>
+              <link href="/ui/style.css" rel="stylesheet" />
+              
+        </head>
+        <body>
+            <div class="container">
+            <div> 
+               <a href="/">Home</a>
+             </div>
+        <br/>
+        <h3>
+           ${heading}
+            </h3>
             <div>
-              ${content}
-            </div>
-            </div>
-    </body>
-</html>
-';
-      return htmlTemplate;
+               ${date}
+                </div>
+                <div>
+                   ${content}
+                </div>
+                </div>
+        </body>
+    </html>
+   ;
+   return createTemplate;
 }
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
  
  app.get('/:articleName', function (req, res) {
-     //articleName = article-one.
-     //articles[articleName] == [] conent for artcle one.
-     var articleName = res.params.articlename;
-     res.send(createTemplate(articles[articleName]));
+     res.send(createTemplate(articleOne));
  });
  
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
 });
