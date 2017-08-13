c01a5cccb94f92bb75c31393920b82f6b8ec6654
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-13 15:18:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,6 +62,12 @@ app.get('/ui/madi.png', function (req, res) {
 
 });
 
+app.get('/ui/main.js', function (req, res) {
+
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
