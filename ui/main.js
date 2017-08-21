2c483d9ba0f41f11e3e3afdb1e1dd20933d7ed41
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-21 18:45:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ buttomn.onclick = function () {
     // Capture the response and store it in a variable
     
     // Render the variable in the  correct span
-    counter = count + 1;
+    counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
     
