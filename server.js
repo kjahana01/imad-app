6d0c34add867f554e3f79c367e6ff6263f772ca8
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-23 00:43:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,11 +63,11 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function(req, res) {
+app.get('/submit-name/:name', function(req, res) {
    // Get the name from the request
    var name = req.params.name;
    
-   name.push(name);
+   names.push(name);
    // JSON: Javascript Object Notation
    res.send(JSON.stringify(names));
 });
