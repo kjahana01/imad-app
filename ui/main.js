9959509634a370ddfddcb79cb8c990616dca2ab1
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-22 23:27:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,19 @@ button.onclick = function () {
     request.open('GET', 'http://kushaljoharapurkar.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+// Student name
+var nameInput = doument.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  // Make a request to the server and send the name.
+  // Capture a list of names and render it as a list.
+  var names = ['name','name2','name3'];
+  var list = '';
+  for(var i=0; i< names.length; i++) {
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
