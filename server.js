1e3e9b63eb702f42b6cffac9920935e35aaff601
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-26 22:30:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 function hash (input, salt) {
     //How do we create a hash?
-    var hashed = crypto.pbkdf25ync(input, salt, 10000, 512, 'sha512' );
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512' );
     return hashed.toString('hex');
 }
 
