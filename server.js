be227655a724173134ab9ab49d12bf378659a94a
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-26 15:50:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,12 +78,12 @@ app.get('/ui/madi.png', function (req, res) {
 
 });
 var pool = new Pool(config);
-app.get('/test-db', function (req, res)  {
-    // make a select request
-    // return a response with the results
-    pool.query('SELECT * FROM test', function(err, result) {
-        if (err) {
-            res.status(500).send(err, toString);
+app.get('/test-db', function (req, res) {
+    //make a select request
+    //return a response with the results
+    pool.query("SELECT * from test", function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
         } else {
             res.send(JSON.stringify(result));
         }
