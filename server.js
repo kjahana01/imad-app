e8def702600e9c0f970a68f9ba4610e63a545267
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-08-28 14:45:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var pool = new Pool(config);
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'kushaljoharapurkar',
@@ -14,6 +15,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title = data.title;
@@ -81,7 +83,23 @@ app.get('/hash/:input', function(req, res){
     
 });
 
-
+app.post('/create-user', function(req, res) {
+   // username , password
+   // ["username": "kushal" , "password": "password"]
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   poll.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+         if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('user successfully created:' , + usernae);
+        }       
+   });  
+});
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
