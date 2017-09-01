09dce81e73ccc31b46afa43c3cb8f86537f708b3
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-09-01 15:51:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,4 @@
-// Counter code
-var button = document.getElementById('counter');
-
-button.onclick = function () {
-    
-    //  Create a request
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange= function () {
-       if (request.readyState === XMLHttpRequest.DONE) {
-           // Take some action 
-            if (request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-       }  
-       // Not done yet
-    };
-   
-    // Make The request
-    request.open('GET', 'http://kushaljoharapurkar.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-// Student name
+// Submit username and password
         var submit = document.getElementById('submit_btn');
         submit.onclick = function () {
     
@@ -32,32 +6,26 @@ button.onclick = function () {
     var request = new XMLHttpRequest();
     
     // Capture the response and store it in a variable
-    request.onreadystatechange= function () {
-       if (request.readyState === XMLHttpRequest.DONE) {
-           // Take some action 
-            if (request.status === 200){
-                  // Capture a list of names and render it as a list.
-                 var names = request.responseText;
-                 names = JSON.parse(names);
-                  var list = '';
-                  for (var i=0; i< names.length; i++) {
-                      list += '<li>' + names[i] + '</li>';
-                  }
-                  var ul = document.getElementById('namelist');
-                  ul.innerHTML = list;
-               
-            }
-       }  
-       // Not done yet
-    };
-   
-    // Make The request
-          var nameInput = document.getElementById('name');
-          var name = nameInput.value;
-    
-          request.open('GET', 'http://kushaljoharapurkar.imad.hasura-app.io/submit-naame?name=' + name, true);
-            request.send(null);
-  // Make a request to the server and send the name.
+   request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.Done) {
+              //take some action
+              if (request.status === 200) {
+                   alert('Logged in successfully');
+              } else if (request.status === 403) {
+                  alert('Username/password is incorrect');
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+              }
+          }
+           //Not done yet
+        };
   
-
+    // Make The request
+          var username = document.getElementById('username').value;
+          var password = document.getElementById('password').value;
+          console.log(username);
+          console.log(password);
+          request.open('POST', 'http://kushaljoharapurkar.imad.hasura-app.io/submit-naame?name=' + name, true);
+          request.setRequestHeader('Content-Type', 'application/json');
+          request.send(JSON.stringify({username: username, password: password}));
 };
