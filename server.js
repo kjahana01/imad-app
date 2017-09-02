9fd6a81721c6b4ceb7722426167c71d335e58580
kjahana01 <30375510+kjahana01@users.noreply.github.com>
2017-09-02 21:22:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,6 +150,11 @@ app.get('/check-login', function(req, res) {
    }
 });
 
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('Logged out');
+});
+
 app.get('/test-db', function (req, res) {
     //make a select request
     //return a response with the results
